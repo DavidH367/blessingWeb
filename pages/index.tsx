59cdb4daa4a3c33234df242d463a11235b966286
David Daniel Fuentez Hernandez <dfuentezhernandez@mcneese.edu>
2025-04-03 18:32:57 -0600

    Actualizacion en pagina principal

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,30 +11,63 @@ import DefaultLayout from "@/layouts/default";
 export default function IndexPage() {
   return (
     <DefaultLayout>
-      <div className="relative">
-      <Image
-        alt="Main Image"
-        src="../img_main.png"
-        radius="none"
-        width="100%"
-        height="400px"
-        style={{ objectFit: "cover" }}
-      />
-      <div className="absolute inset-0 flex items-center justify-center z-10">
-        <h1
-          className="text-white text-4xl font-bold"
-          style={{ fontFamily: "'Playfair Display', serif" }}
-        >
-          New Life Project
-        </h1>
+      <div className="relative opacity-90">
+        <Image
+          alt="Main Image"
+          src="../img_main.png"
+          radius="none"
+          width="100%"
+          height="400px"
+          style={{ objectFit: "cover" }}
+        />
+        <div className="absolute inset-0 flex items-center justify-center z-10">
+          <h1
+            className="text-white text-6xl font-extrabold text-center "
+            style={{
+              fontFamily: "'Playfair Display', serif",
+              textShadow: "2px 2px 4px rgba(0, 0, 0, 0.6)", // Sombra alrededor del texto
+            }}
+          >
+            New Life Project
+          </h1>
+        </div>
       </div>
-    </div>
       <section>
-        <div className="container mx-auto max-w-full flex-grow pt-2 sm:m-4 sm:px-6 md:m-12 ">
-          <p className="text-center text-justify font-bold text-2xl m-6">Our reason to exist: To help children in Comayagua in general educational and biblical formation.</p>
-          <p className="text-center text-justify mt-2 text-lg m-6">The New Life Project is a ministry whose vision is to transform children in an integral way and in this way change their lives and give them the necessary knowledge so that these children can change their lives firstly in the spiritual area and collaterally in the rest, and so that these children can be examples in their country, their family, and their community.</p>
+        <div className="container max-w-full flex-grow tracking-wide pb-32 py-14 px-10 md:px-20 lg:px-50 xl:px-40 2xl:px-80">
+          <p className="text-center text-justify font-bold text-2xl">Our reason to exist: To help children in Honduras in general educational and biblical formation.</p>
+          <p className="text-center text-justify text-xl py-6 tracking-normal">The New Life Project is a ministry whose vision is to transform children in an integral way and in this way change their lives and give them the necessary knowledge so that these children can change their lives firstly in the spiritual area and collaterally in the rest, and so that these children can be examples in their country, their family, and their community.</p>
         </div>
       </section>
+      <div className="relative opacity-90">
+        <Image
+          alt="Main Image"
+          src="../img_second.png"
+          radius="none"
+          width="100%"
+          height="600px"
+          style={{ objectFit: "cover" }}
+        />
+        <div className="absolute inset-0 flex flex-col items-center justify-center text-center z-10 h-[600px]">
+          <h1
+            className="text-white text-2xl md:text-6xl font-extrabold"
+            style={{
+              fontFamily: "'Playfair Display', serif",
+              textShadow: "2px 2px 4px rgba(0, 0, 0, 0.6)", // Sombra alrededor del texto
+            }}
+          >
+            Kids and Education
+          </h1>
+          <div className="container max-w-full tracking-wide py-8 px-6 md:pb-8 md:py-14 md:px-10 lg:px-20 xl:px-40 2xl:px-[400px]">
+            <p className="font-bold text-lg md:text-2xl text-white">
+              La educación es un proceso continuo que involucra tanto a la escuela como a la familia. Cuando ambos trabajan juntos, los niños y jóvenes desarrollan mejor sus habilidades intelectuales, emocionales y sociales.
+            </p>
+            <p className="text-justify text-md md:text-xl py-6 tracking-normal text-white">
+              Esta alianza beneficia a los estudiantes a que mejoren su rendimiento y adaptación, los padres entienden mejor el desarrollo de sus hijos, y las escuelas crean un mejor ambiente de aprendizaje. Los padres tienen un rol clave en la prevención de problemas y en el apoyo educativo.
+            </p>
+           
+          </div>
+        </div>
+      </div>
     </DefaultLayout>
   );
 }
